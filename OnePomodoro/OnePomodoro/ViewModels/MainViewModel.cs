a3b8c0eb7b640b74d9723ae38dc52ea4f399808c
DinoChan <dino.chan@live.com>
2019-06-21 11:01:08 +0800

    ....

---
+++ OnePomodoro/OnePomodoro/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Prism.Windows.Mvvm;
+
+namespace OnePomodoro.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
